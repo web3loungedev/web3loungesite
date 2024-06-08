5cf65b525aeea1c5d4e5f9e02fa261b5298a0c74
Александр Рычагов <alexandre.rychagov@gmail.com>
2024-06-08 18:49:16 +0500

    feat: add coin360 widget with query params

---
+++ apps/special-fortnight/app/widgets/coin360/coin360.widget.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
+const DEFAULT_COIN = 'bitcoin-btc';
+
+export function Coin360Widget() {
+  const params = useSearchParams();
+
+  const coin = params.get('coin') ?? DEFAULT_COIN;
+
+  return (
+    <iframe
+      src={`https://coin360.com/coin-widget?coin=${coin}&utm_source=embed_widget_coin`}
+      width="100%"
+      height="100%"
+      frameBorder="0"
+    />
+  );
+}
---
+++ apps/special-fortnight/app/widgets/coin360/page.tsx
@@ -0,0 +1,9 @@
+import { Coin360Widget } from './coin360.widget';
+
+export default function Page() {
+  return (
+    <div className="h-screen">
+      <Coin360Widget />
+    </div>
+  );
+}
