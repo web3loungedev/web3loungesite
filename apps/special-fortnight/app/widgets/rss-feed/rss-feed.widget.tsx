ed297bd776448c963f26b7826a1a8b2d090397a1
Александр Рычагов <alexandre.rychagov@gmail.com>
2024-06-09 17:28:37 +0500

    feat: add header to rss feed

--- apps/special-fortnight/app/widgets/rss-feed/rss-feed.widget.tsx
+++ apps/special-fortnight/app/widgets/rss-feed/rss-feed.widget.tsx
@@ -41,23 +41,39 @@ export function RssFeedWidget() {
   const data = useRssFeed({ id });
 
   return (
-    <div className="max-h-full w-full overflow-y-auto overflow-x-hidden bg-bg-primary flex flex-col gap-4 p-4 place-items-center">
-      {data?.items.map((item) => {
-        const renderer = resolveRendererForItem(item);
+    <div className="max-h-full w-full bg-bg-primary flex flex-col">
+      <div className="shadow-md">
+        <div className="flex p-4 m-4 gap-4 bg-white border-slate-300 border-2  shadow-sm">
+          {data?.image.url && (
+            <div>
+              <img src={data?.image.url} alt={data.image.title} />
+            </div>
+          )}
+          {data?.title && (
+            <div className="flex">
+              <p className="font-semibold">{data?.title}</p>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="max-h-full w-full overflow-y-auto overflow-x-hidden flex flex-col gap-4 p-4 place-items-center shadow-inner">
+        {data?.items.map((item) => {
+          const renderer = resolveRendererForItem(item);
 
-        if (renderer == null) {
-          return null;
-        }
+          if (renderer == null) {
+            return null;
+          }
 
-        return (
-          <div
-            key={item.guid}
-            className="max-w-2xl w-full m-4 min-w p-4 bg-white"
-          >
-            {renderer(item)}
-          </div>
-        );
-      })}
+          return (
+            <div
+              key={item.guid}
+              className="max-w-2xl w-full min-w m-4 p-4 bg-white shadow-sm"
+            >
+              {renderer(item)}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
